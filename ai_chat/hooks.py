1ade2c22e6b682ea6e0d73b53bacdb8e528984e4
Jem Pablo <admin@jempablo.com>
2025-04-25 10:15:46 -0700

    changed hooks py

--- ai_chat/hooks.py
+++ ai_chat/hooks.py
@@ -7,7 +7,7 @@ app_license = "MIT"
 
 app_include_js = "/assets/ai_chat/js/chat_widget.js"
 web_include_js = "/assets/ai_chat/js/chat_widget.js"
-override_whitelisted_methods = {
-    "frappe.auth.get_logged_user": "ai_chat.ai_chatbot.api.auth.get_logged_user_safe"
-}
 
+override_whitelisted_methods = {
+    "frappe.auth.get_logged_user": "ai_chat.ai_chat.ai_chatbot.api.auth.get_logged_user_safe"
+}
