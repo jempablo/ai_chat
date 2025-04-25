d53c78ce30efa8a9b639e58ccbab9b55de0aab05
Jem Pablo <admin@jempablo.com>
2025-04-24 18:13:51 -0700

    ✅ more changes on hooks py

--- ai_chat/hooks.py
+++ ai_chat/hooks.py
@@ -1,5 +1,9 @@
-# Inject in Desk (Logged-in users)
-app_include_js = "/assets/ai_chat/js/chat_widget.js"
+app_name = "ai_chat"
+app_title = "AI Chatbot"
+app_publisher = "Jem Pablo"
+app_description = "Chatbot App"
+app_email = "admin@jempablo.com"
+app_license = "MIT"
 
-# Inject in Website (Public-facing)
+app_include_js = "/assets/ai_chat/js/chat_widget.js"
 web_include_js = "/assets/ai_chat/js/chat_widget.js"
